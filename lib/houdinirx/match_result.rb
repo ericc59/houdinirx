cffb193daada7a8e0c8922da6a83539e3033e414
Eric Campbell <eric@earthaid.net>
2011-08-03 10:39:38 -0400

    adds a to_hash method to retrieve captures as a hash

--- lib/houdinirx/match_result.rb
+++ lib/houdinirx/match_result.rb
@@ -10,16 +10,30 @@ module Houdini
       @captures = captures.collect {|c| c.to_sym }
     end
     
+    def to_hash
+      {}.tap do |attributes|
+        captures.each { |c| attributes[c] = value_for(c) }
+      end
+    end
+    
     def method_missing(method, *args, &block)
-      if @captures.include?(method)
-        index = @captures.index(method)
-        if @match.class == MatchData
-          @match.captures[index]
-        else
-          @match[index]
-        end
+      value = value_for(method)
+      
+      if value
+        return value
       else
         raise RuntimeError, "#{method} not captured"
+      end
+    end
+    
+    protected
+    def value_for(name)
+      index = @captures.index(name)
+      return nil if index == nil
+      if @match.class == MatchData
+        @match.captures[index]
+      else
+        @match[index]
       end
     end
     
